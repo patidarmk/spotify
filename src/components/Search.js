23babeb1a7327fbfff362caf9a5d6d4d5af28e61
unknown <patidarmukesh123@gmail.com>
2023-03-11 20:50:35 +0530

    first commit

--- src/components/Search.js
+++
@@ -1,9 +0,0 @@
-const Search = () => {
-    return (
-        <div>
-           <input type="text" className='search-box' placeholder = "Search Song,Artist" />
-        </div>
-    )
-}
-
-export default Search;
--- src/hooks/navigation.js
+++
@@ -1,9 +0,0 @@
-import { gql } from "@apollo/client";
-
-export const GET_PLAYLISTS = gql`
-query GetPlaylists {
-  getPlaylists {
-    id
-    title
-  }
-}`
